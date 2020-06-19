e3c109d35317d50e858527c18536c94261206167
unknown <16740768+Lucy-SD-Developer@users.noreply.github.com>
2020-06-19 18:57:20 -0400

    Update

--- jsFiles/exercises-4.js
+++ jsFiles/exercises-4.js
@@ -12,8 +12,7 @@ function myFunc1() {
         txt = " is correct!";
     } else if (curr == "") {
         alert("Please choose one option.")
-    } 
-    else {
+    } else {
         txt = " is not correct...";
     }
     document.getElementById("divOutput1").innerHTML = "Your choice: " + curr + txt;
@@ -33,11 +32,10 @@ function myFunc2() {
         txt = " is correct!";
     } else if (curr == "") {
         alert("Please choose one option.")
-    } 
-    else {
+    } else {
         txt = " is not correct...";
     }
-    document.getElementById("output2").value = "Your choice: " + curr + txt;
+    document.getElementById("divOutput2").innerHTML = "Your choice: " + curr + txt;
 }
 
 function myFunc3() {
@@ -54,20 +52,101 @@ function myFunc3() {
         txt = " is correct!";
     } else if (curr == "") {
         alert("Please choose one option.")
-    } 
-    else {
+    } else {
         txt = " is not correct...";
     }
-    document.getElementById("output3").value = "Your choice: " + curr + txt;
+    document.getElementById("divOutput3").innerHTML = "Your choice: " + curr + txt;
 }
 
-function myFuncX() {
-    var twoObjText = document.getElementById("inputX").value;
-    console.log(twoObjText);
-    if (twoObjText == "25 125") {
-        document.getElementById("outputX").innerHTML = "That is correct!"
+function myFunc4() {
+    var currForm = document.forms[3];
+    var curr = "";
+    var i;
+    var txt;
+    for (i = 0; i < currForm.length; i++) {
+        if (currForm[i].checked) {
+        curr = currForm[i].value;
+        }
+    }
+    if (curr == "c") {
+        txt = " is correct!";
+    } else if (curr == "") {
+        alert("Please choose one option.")
     } else {
-        document.getElementById("outputX").innerHTML = "Incorrect. Please try again."
+        txt = " is not correct...";
+    }
+    document.getElementById("divOutput4").innerHTML = "Your choice: " + curr + txt;  
+}
+
+function myFunc5() {
+    var currForm = document.forms[4];
+    var curr = "";
+    var i;
+    var txt;
+    for (i = 0; i < currForm.length; i++) {
+        if (currForm[i].checked) {
+        curr = currForm[i].value;
+        }
+    }
+    if (curr == "a") {
+        txt = " is correct!";
+    } else if (curr == "") {
+        alert("Please choose one option.")
+    } else {
+        txt = " is not correct...";
+    }
+    document.getElementById("divOutput5").innerHTML = "Your choice: " + curr + txt;  
+}
+
+function myFunc6() {
+    var currForm = document.forms[5];
+    var curr = "";
+    var i;
+    var txt;
+    for (i = 0; i < currForm.length; i++) {
+        if (currForm[i].checked) {
+        curr = currForm[i].value;
+        }
+    }
+    if (curr == "d") {
+        txt = " is correct!";
+    } else if (curr == "") {
+        alert("Please choose one option.")
+    } else {
+        txt = " is not correct...";
+    }
+    document.getElementById("divOutput6").innerHTML = "Your choice: " + curr + txt;   
+}
+
+function myFunc7() {
+    var currForm = document.forms[6];
+    var curr = "";
+    var i;
+    var txt;
+    for (i = 0; i < currForm.length; i++) {
+        if (currForm[i].checked) {
+        curr = currForm[i].value;
+        }
+    }
+    if (curr == "b") {
+        txt = " is correct!";
+    } else if (curr == "") {
+        alert("Please choose one option.")
+    } else {
+        txt = " is not correct...";
+    }
+    document.getElementById("divOutput7").innerHTML = "Your choice: " + curr + txt;   
+}
+
+function myFunc2_1() {
+    var currObjText = document.getElementById("input2_1").value;
+    console.log(currObjText);
+    if (currObjText == "25 125") {
+        document.getElementById("divOutput2_1").innerHTML = "That is correct!"
+    } else if (currObjText == "") {
+        alert("Please input the result.")
+    } else {
+        document.getElementById("divOutput2_1").innerHTML = "Incorrect. Please try again."
     }
 }
 
